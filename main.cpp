6e0b1f6c13d4d99c916b4a66b46e6753a6b19f1f
ewalker0512 <60410770+ewalker0512@users.noreply.github.com>
2021-10-04 20:04:59 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,51 @@
 #include <iostream>
-#include <vector>
-
+#include <ctime>
+#include <cmath>
 using namespace std;
 
-class Student{
+class Die{ //Class die with an int member variable for face.
 private:
-    int numGrades;
-    int grade;
+    int face; //Member variable with name face.
 public:
-    Student(){
-    numGrades = 0;
-    grade = 0;
-    }
-    Student(int nG, int g){
-        numGrades = nG;
-        grade = g;
-    }
-    Student(int g){
-        grade = g;
-    }
-    getGrade(){return grade;}
-    getNumGrades(){return numGrades;}
-    void setGrade(int g){grade = g;}
-    void setNumGrades(int nG){numGrades = nG;}
-
-    /*int[] createArray(int numGrades){
-        int gradeArr[numGrades];
-        return gradeArr;
-    }
-    int highestNumber(int n)
-    void storeNumber(int g){
-        push_back(g);
-    }*/
-
+    Die(int f);
+    friend void print(const Die &f);
+    friend ostream &operator<<(ostream &, const Die &);
+    friend istream &operator>>(istream&, Die &);
 };
-
-int main()
-{
-    int grades[0];
-    Student s1 (3,95);
-    cout << "Student 1's grade was " << s1.getGrade()<< endl;
-    cout << "Student 1 has " << s1.getNumGrades() << " grades for the semester." << endl;
-
-
-    return 0;
+    Die::Die(int f){
+        face = f;
+    }
+    ostream &operator<<(ostream &strm, const Die &d) { //Overload of the << operator.
+    strm << "This die face value is " << d.face;
+    return strm;
+    }
+    istream &operator>>(istream &strm, Die &f){ //Overload of the >> operator.
+    cout << "What was the value of this die : ";
+    strm >> f.face;
+    return strm;
+    }
+    void print(const Die &d){//Friend function that prints the value of a Die object.
+        cout << d.face << endl;
+    }
+int main(){
+srand(time(0));
+Die dieOne(rand() % 6 + 1); //The member variable face with the value between 1 and 6.
+Die dieTwo(rand() % 6 + 1);
+Die dieThree(rand() % 6 + 1);
+print(dieOne); //Test of the print friend function
+print(dieTwo);
+cout << dieOne << endl; //Test of the overloaded << operator.
+cout << dieTwo << endl;
+cout << dieThree << endl;
+cin >> dieThree;
+cout << "The third die face now reads : " << dieThree<< endl;
+return 0;
 }
+
+/*
+Create a Die class that represents one dice.
+The only member variable is face which can have a value between 1 and 6.
+Create a constructor where the parameter is assigned to face.
+Create a friend function that prints the value of a Die object.
+Overload the <<, >>, and = operators for the Die class and demonstrate all these functions in a program.
+*/
